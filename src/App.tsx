a868cd2f2666ac7e37aefb942cbb9b0a73881e13
DavidNogu <hsd_sk8@hotmail.com>
2024-11-25 13:10:46 +0100

    add text examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Button } from '@manamerge/mana-atomic-ui';
+import React from "react";
+import "./App.css";
+import { Button } from "@manamerge/mana-atomic-ui";
+import { Text } from "@manamerge/mana-atomic-ui";
 
 function App() {
-    return (
-        <div className="App">
-            <Button className="primary">Name of the Button</Button>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Button className="primary">Button secondary primary</Button>
+      <Button className="secondary">Button secondary</Button>
+
+      <Text className="light">Gilmer font here!</Text>
+      <Text className="regular">Gilmer font here!</Text>
+      <Text className="medium">Gilmer font here!</Text>
+      <Text className="bold">Gilmer font here!</Text>
+      <Text className="heavy">Gilmer font here!</Text>
+    </div>
+  );
 }
 
 export default App;
