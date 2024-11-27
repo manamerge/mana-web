941ba3be6721e91931c7e13cd5f893b5cdd124ca
Alexis <alexis.sousa@giffits.de>
2024-11-27 17:53:27 +0100

    feat: :heavy_plus_sign: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+    {
+        ignores: ["build", "dist"]
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintConfigPrettier
+];
