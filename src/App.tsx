51401abec642be52a15f24040181e2cd965b8d9a
Alexis <alexis.sousa@giffits.de>
2025-03-07 16:20:25 +0100

    feat: :sparkles: add molecule Header and Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import "./App.css";
 import React from "react";
-import { Button } from "@manamerge/mana-atomic-ui";
-import { Text } from "@manamerge/mana-atomic-ui";
-import { Link } from "@manamerge/mana-atomic-ui";
-import { Heading } from "@manamerge/mana-atomic-ui";
-import { Icon } from "@manamerge/mana-atomic-ui";
+import {
+    Button,
+    Text,
+    Link,
+    Heading,
+    Icon,
+    Navigation,
+    Header
+} from "@manamerge/mana-atomic-ui";
+
+import { ReactComponent as iconBurger } from "./icons/burger.svg";
+import { ReactComponent as iconTop } from "./icons/iconTop.svg";
+import { ReactComponent as iconDot } from "./icons/iconDot.svg";
+import { ReactComponent as iconLeft } from "./icons/iconLeft.svg";
+import { ReactComponent as iconRight } from "./icons/iconRight.svg";
+
 import { ReactComponent as logo } from "./logo.svg";
 function App() {
     return (
         <div className="App">
+            <Navigation
+                iconBurger={iconBurger}
+                onTabClick={() => {}}
+                tabs={[
+                    {
+                        label: "Home",
+                        to: "/Home"
+                    },
+                    {
+                        label: "About",
+                        to: "/About"
+                    },
+                    {
+                        label: "Contact",
+                        to: "/Contact"
+                    }
+                ]}
+            />
             <div
                 style={{
                     display: "flex",
@@ -19,41 +48,58 @@ function App() {
                     width: "full"
                 }}
             >
-                <Icon className="small" svg={logo} />
-                <Icon className="medium" svg={logo} />
-                <Icon className="large" svg={logo} />
-                <Heading className="h1">H1 in the ZONE!</Heading>
-                <Heading className="h2">H2 in the ZONE!</Heading>
-                <Heading className="h3">H3 in the ZONE!</Heading>
-                <Heading className="h4">H4 in the ZONE!</Heading>
-                <Heading className="h5">H5 in the ZONE!</Heading>
-                <Heading className="h6">H6 in the ZONE!</Heading>
+                <Header
+                    buttonLeftClick={() => {}}
+                    buttonLeftText="Let's talk"
+                    buttonRightClick={() => {}}
+                    buttonRightText="Contact us"
+                    headingTitle="Welcome to ManaMerge"
+                    iconDot={iconDot}
+                    iconLeft={iconLeft}
+                    iconRight={iconRight}
+                    iconTop={iconTop}
+                    textBanner="available to offer services"
+                    textTitle={[
+                        "We are a team of Software Engineers",
+                        "Our interest goes to all developing environments"
+                    ]}
+                />
 
-                <Link href="https://www.google.com" className="light">
+                <Icon variant="small" svg={logo} />
+                <Icon variant="medium" svg={logo} />
+                <Icon variant="large" svg={logo} />
+                <Heading variant="h1">H1 in the ZONE!</Heading>
+                <Heading variant="h2">H2 in the ZONE!</Heading>
+                <Heading variant="h3">H3 in the ZONE!</Heading>
+                <Heading variant="h4">H4 in the ZONE!</Heading>
+                <Heading variant="h5">H5 in the ZONE!</Heading>
+                <Heading variant="h6">H6 in the ZONE!</Heading>
+
+                <Link href="https://www.google.com" variant="light">
                     Link here!
                 </Link>
-                <Link href={"https://www.google.com"} className="regular">
+                <Link href={"https://www.google.com"} variant="regular">
                     Link here!
                 </Link>
-                <Link href={"https://www.google.com"} className="medium">
+                <Link href={"https://www.google.com"} variant="medium">
                     Link here!
                 </Link>
-                <Link href={"https://www.google.com"} className="bold">
+                <Link href={"https://www.google.com"} variant="bold">
                     Link here!
                 </Link>
-                <Link href={"https://www.google.com"} className="heavy">
+                <Link href={"https://www.google.com"} variant="heavy">
                     Link here!
                 </Link>
             </div>
 
-            <Button className="primary">Button secondary primary</Button>
-            <Button className="secondary">Button secondary</Button>
+            <Button variant="primary">Button secondary primary</Button>
+            <Button variant="secondary">Button secondary</Button>
 
-            <Text className="light">Gilmer font here!</Text>
-            <Text className="regular">Gilmer font here!</Text>
-            <Text className="medium">Gilmer font here!</Text>
-            <Text className="bold">Gilmer font here!</Text>
-            <Text className="heavy">Gilmer font here!</Text>
+            <Text variant="light">Gilmer font here!</Text>
+            <Text variant="regular">Gilmer font here!</Text>
+            <Text variant="medium">Gilmer font here!</Text>
+            <Text variant="bold">Gilmer font here!</Text>
+            <Text variant="heavy">Gilmer font here!</Text>
         </div>
     );
 }
