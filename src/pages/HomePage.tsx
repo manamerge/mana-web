7094493946b4968352df51eb1e3f2e5cb3bcbd6b
DavidNogu <hsd_sk8@hotmail.com>
2025-04-09 15:26:45 +0100

    add link to visit test components page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return <div>HomePage</div>;
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between"
+            }}
+        >
+            <div>HomePage</div>
+            <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
+                <div>Visit components page:</div>
+                <Link to="/test">components page</Link>
+            </div>
+        </div>
+    );
 }
