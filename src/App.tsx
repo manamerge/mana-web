8878d4c55e6df552b97a42f4908c2abdd0007f8f
DavidNogu <hsd_sk8@hotmail.com>
2025-04-09 15:02:24 +0100

    refactor classname and add gradient

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { Text } from "@manamerge/mana-atomic-ui";
 import { Link } from "@manamerge/mana-atomic-ui";
 import { Heading } from "@manamerge/mana-atomic-ui";
 import { Icon } from "@manamerge/mana-atomic-ui";
+import { Gradient } from "@manamerge/mana-atomic-ui";
 import { ReactComponent as logo } from "./logo.svg";
+
 function App() {
     return (
         <div className="App">
@@ -19,15 +21,51 @@ function App() {
                     width: "full"
                 }}
             >
-                <Icon className="small" svg={logo} />
-                <Icon className="medium" svg={logo} />
-                <Icon className="large" svg={logo} />
-                <Heading className="h1">H1 in the ZONE!</Heading>
-                <Heading className="h2">H2 in the ZONE!</Heading>
-                <Heading className="h3">H3 in the ZONE!</Heading>
-                <Heading className="h4">H4 in the ZONE!</Heading>
-                <Heading className="h5">H5 in the ZONE!</Heading>
-                <Heading className="h6">H6 in the ZONE!</Heading>
+                <Gradient variant="primary">
+                    <Text variant="light">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Culpa minima aspernatur officiis, saepe aliquid
+                        possimus expedita ad ut sint fugiat?
+                    </Text>
+                </Gradient>
+                <Gradient variant="secondary">
+                    <Text variant="regular">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Culpa minima aspernatur officiis, saepe aliquid
+                        possimus expedita ad ut sint fugiat?
+                    </Text>
+                </Gradient>
+                <Gradient variant="tertiary">
+                    <Text variant="medium">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Culpa minima aspernatur officiis, saepe aliquid
+                        possimus expedita ad ut sint fugiat?
+                    </Text>
+                </Gradient>
+                <Gradient variant="primary">
+                    <Text variant="bold">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Culpa minima aspernatur officiis, saepe aliquid
+                        possimus expedita ad ut sint fugiat?
+                    </Text>
+                </Gradient>
+                <Gradient variant="secondary">
+                    <Text variant="heavy">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Culpa minima aspernatur officiis, saepe aliquid
+                        possimus expedita ad ut sint fugiat?
+                    </Text>
+                </Gradient>
+
+                <Icon variant="small" svg={logo} />
+                <Icon variant="medium" svg={logo} />
+                <Icon variant="large" svg={logo} />
+                <Heading variant="h1">H1 in the ZONE!</Heading>
+                <Heading variant="h2">H2 in the ZONE!</Heading>
+                <Heading variant="h3">H3 in the ZONE!</Heading>
+                <Heading variant="h4">H4 in the ZONE!</Heading>
+                <Heading variant="h5">H5 in the ZONE!</Heading>
+                <Heading variant="h6">H6 in the ZONE!</Heading>
 
                 <Link href="https://www.google.com" className="light">
                     Link here!
@@ -46,14 +84,14 @@ function App() {
                 </Link>
             </div>
 
-            <Button className="primary">Button secondary primary</Button>
-            <Button className="secondary">Button secondary</Button>
+            <Button variant="primary">Button secondary primary</Button>
+            <Button variant="secondary">Button secondary</Button>
 
-            <Text className="light">Gilmer font here!</Text>
-            <Text className="regular">Gilmer font here!</Text>
-            <Text className="medium">Gilmer font here!</Text>
-            <Text className="bold">Gilmer font here!</Text>
-            <Text className="heavy">Gilmer font here!</Text>
+            <Text variant="light">Gilmer font here!</Text>
+            <Text variant="regular">Gilmer font here!</Text>
+            <Text variant="medium">Gilmer font here!</Text>
+            <Text variant="bold">Gilmer font here!</Text>
+            <Text variant="heavy">Gilmer font here!</Text>
         </div>
     );
 }
