f33cc282cc294457316962853b149c5e8138556f
AlexAxis <alexis-sousa@hotmail.com>
2025-12-12 15:49:44 +0100

    feat: :sparkles: add accordion component + add new storybook theme

--- src/utils/Theme/index.ts
+++ src/utils/Theme/index.ts
@@ -1,5 +1,11 @@
 //@ts-nocheck
-export { manamergeTheme as default } from "@manamerge/mana-atomic-ui/dist/themes/themes/Manamerge";
+export { ManamergeTheme as default } from "@manamerge/mana-atomic-ui/dist/themes/themes/ManamergeTheme";
 
 //@ts-nocheck
 export { ManamergeStyles as GlobalStyles } from "@manamerge/mana-atomic-ui/dist/themes/themes/manamerge/fonts";
+
+// //@ts-nocheck
+// export { DavidWebTheme as default } from "@manamerge/mana-atomic-ui/dist/themes/themes/DavidWebTheme";
+
+// //@ts-nocheck
+// export { DavidWebStyles as GlobalStyles } from "@manamerge/mana-atomic-ui/dist/themes/themes/davidWeb/fonts";
