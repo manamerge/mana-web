71adaadf8100e16298e9d578f338d8ee462b94b4
AlexAxis <alexis-sousa@hotmail.com>
2026-02-26 18:31:22 +0100

    page 404

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Link } from "@manamerge/mana-atomic-ui";
+import { Link, Icon, Heading } from "@manamerge/mana-atomic-ui";
+import { StyledHeading } from "./NotFoundPage.css";
+import Green from "./assets/BigGreen.png";
+import { ReactComponent as ImageNotFound } from "./assets/ImageNotFound.svg";
 
 export default function NotFoundPage() {
     return (
-        <div>
-            404. Page not found... Go <Link to="/">Home</Link>;
+        <div
+            style={{
+                marginBottom: "50px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "20px"
+            }}
+        >
+            <StyledHeading variant="secondary" asTag="h1">
+                ... page not found ...
+            </StyledHeading>
+            <Link
+                variant="primary-heavy"
+                to="/"
+                style={{ margin: "10px 0 10px 0" }}
+            >
+                take me to the home page
+            </Link>
+            <Icon svg={ImageNotFound} variant="xxxxl" size="500px" />;
         </div>
     );
 }
