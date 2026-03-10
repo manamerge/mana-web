4202e1ca681467f0d4b8ddef7ab5c92f1f2a8908
DavidNogu <hsd_sk8@hotmail.com>
2026-03-10 16:23:07 +0000

    adapt backdrop-navigation

--- src/layouts/Layout.css.ts
+++ src/layouts/Layout.css.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const NavigationWrapper = styled.div`
+    .Backdrop-Navigation {
+        z-index: 2;
+    }
+
     .Navigation-primary {
         z-index: 2;
     }
